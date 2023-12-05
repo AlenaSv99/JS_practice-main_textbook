88e5379e35ddbf3be91af5ea692dc4a7cbf12adb
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2023-12-05 18:52:13 +0300

    Add files via upload

---
+++ Основы/⊗jsPmBsMOV/script2.js
@@ -0,0 +1,8 @@
+// 2 задача
+
+let c = 10;
+let d = 5;
+let result = a + b; 
+alert(result);
+
+// выведется 15.
---
+++ Основы/⊗jsPmBsTS/script.js
@@ -0,0 +1,10 @@
+// Перепишите следующий код через вставку переменных:
+/*
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc';
+*/
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa ${str1} bbb ${str2} ccc`;
