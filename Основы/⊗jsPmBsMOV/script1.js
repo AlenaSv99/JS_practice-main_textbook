59fdf8b4c4ab8b756c2961a142761f5e10bdcf19
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2023-12-09 17:19:10 +0300

    Delete Основы/⊗jsPmBsMOV directory

--- Основы/⊗jsPmBsMOV/script1.js
+++
@@ -1,9 +0,0 @@
-// 1 задача
-
-let a = 10;
-let b = 2;
-alert(a + b); // 12
-alert(a - b); // 8
-alert(a * b); // 20
-alert(a / b); // 5
-
--- Основы/⊗jsPmBsMOV/script2.js
+++
@@ -1,8 +0,0 @@
-// 2 задача
-
-let c = 10;
-let d = 5;
-let result = a + b; 
-alert(result);
-
-// выведется 15.
