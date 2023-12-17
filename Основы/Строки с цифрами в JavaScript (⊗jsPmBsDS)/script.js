d7c10c45700809a608178d879104623b8dfcc9f6
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2023-12-17 22:26:59 +0300

    Add files via upload

---
+++ Основы/Строки с цифрами в JavaScript (⊗jsPmBsDS)/script.js
@@ -0,0 +1,4 @@
+// Дана строка '12345'. Найдите сумму цифр этой строки.
+
+let str = '12345';
+alert(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4])); // выведется 15.
