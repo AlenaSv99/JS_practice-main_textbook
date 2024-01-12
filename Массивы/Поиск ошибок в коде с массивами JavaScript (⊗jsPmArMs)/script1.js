44aaa889dc5ffdd1bcdffcedf7d15b908e436c29
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-12 22:57:28 +0300

    Add files via upload

---
+++ Массивы/Поиск ошибок в коде с массивами JavaScript (⊗jsPmArMs)/script1.js
@@ -0,0 +1,9 @@
+// Код должен вывести последний элемент массива:
+
+/*
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length]); // выведет undefined
+*/
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length - 1]); //выведет 5
---
+++ Массивы/Поиск ошибок в коде с массивами JavaScript (⊗jsPmArMs)/script4.js
@@ -0,0 +1,9 @@
+// Код должен вывести длину массива:
+
+/*
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.lenght);
+*/
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length); // выведет 5
