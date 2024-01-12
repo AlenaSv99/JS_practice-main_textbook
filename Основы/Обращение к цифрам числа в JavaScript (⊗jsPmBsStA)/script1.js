b3b7b2573db45a902973ab3d4b55694998c007bd
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-12 20:04:52 +0300

    Add files via upload

---
+++ Основы/Обращение к цифрам числа в JavaScript (⊗jsPmBsStA)/script1.js
@@ -0,0 +1,4 @@
+// Дано число 12345. Найдите сумму цифр этого числа.
+
+let test = String(12345); // перевод числа в строку
+alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4])); // выведет 15
