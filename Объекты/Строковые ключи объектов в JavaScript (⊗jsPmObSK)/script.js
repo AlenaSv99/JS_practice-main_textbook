38eac03aee9eda1588502a07662914a3cefe1be1
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-12 23:25:05 +0300

    Add files via upload

---
+++ Объекты/Строковые ключи объектов в JavaScript (⊗jsPmObSK)/script.js
@@ -0,0 +1,5 @@
+// Создайте объект user с ключами 'name', 'surname', 'patronymic' и какими-то произвольными значениями. 
+// Выведите на экран фамилию, имя и отчество через пробел.
+
+let obj = {name: 'Boris', surname: 'Vasylev', patronymic: 'Olegovich'};
+console.log(obj['name'], obj['surname'], obj['patronymic']); // выведет Boris Vasylev Olegovich
