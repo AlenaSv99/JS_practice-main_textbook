ed39778479bd5e79478430ecef5c1c710b5cba97
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-13 14:40:52 +0300

    Add files via upload

---
+++ Объекты/Ошибка обращения к элементу по ключу в JavaScript (⊗jsPmObEAM)/script1.js
@@ -0,0 +1,9 @@
+// Исправьте ошибку, допущенную в следующем коде:
+
+/*
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj[x]); // выведет Uncaught ReferenceError: x is not defined
+*/
+
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj['x']);
---
+++ Объекты/Ошибка обращения к элементу по ключу в JavaScript (⊗jsPmObEAM)/script2.js
@@ -0,0 +1,11 @@
+// Исправьте ошибку, допущенную в следующем коде:
+
+/*
+let obj = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj['key']); // выведет undefined
+*/
+
+let obj = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj[key]); // выведет 1
