de18f4bd4e6ef2920d469f9fa42234ebd434fc1c
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-13 14:52:46 +0300

    Add files via upload

---
+++ Объекты/Ошибка обращения к свойству объекта в JavaScript (⊗jsPmObPAM)/script1.js
@@ -0,0 +1,13 @@
+// Исправьте ошибку, допущенную в следующем коде:
+
+/*
+let obj = {x: 1, y: 2, z: 3};
+
+let prop = 'x';
+console.log(obj.prop); // выведет undefined
+*/
+
+let obj = {x: 1, y: 2, z: 3};
+
+let prop = 'x';
+console.log(obj[prop]); // выведет 1
