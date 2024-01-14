6113c3be02024f92d3cceffbe3aa283375b1b5db
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-14 17:30:38 +0300

    Add files via upload

---
+++ Объекты/Подход программирования через константы в JavaScript (⊗jsPmObCPW)/script.js
@@ -0,0 +1,13 @@
+// Перепишите следующий код через описанный подход:
+
+/*
+let arr = [1, 2, 3, 4, 5];
+let res = arr[1] + arr[2];
+
+console.log(res); // выведет 5
+*/
+
+const arr = [1, 2, 3, 4, 5];
+const res = arr[1] + arr[2];
+
+console.log(res); // выведет 5
