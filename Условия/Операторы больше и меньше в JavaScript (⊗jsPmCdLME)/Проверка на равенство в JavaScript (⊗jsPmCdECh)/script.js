0378bf689cb5bd80a0393387eaabfd5586c15c90
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-18 17:01:42 +0300

    Delete Условия/Операторы больше и меньше в JavaScript (⊗jsPmCdLME)/Проверка на равенство в JavaScript (⊗jsPmCdECh) directory

--- Условия/Операторы больше и меньше в JavaScript (⊗jsPmCdLME)/Проверка на равенство в JavaScript (⊗jsPmCdECh)/script.js
+++
@@ -1,9 +0,0 @@
-// Проверьте, что переменная test равна 10.
-
-let test = Number(prompt('Введите число'));
-
-if (test == 10) {
-	console.log(String(test) + ' равно 10'); 
-} else {
-	console.log(String(test) + ' не равно 10');
-}
