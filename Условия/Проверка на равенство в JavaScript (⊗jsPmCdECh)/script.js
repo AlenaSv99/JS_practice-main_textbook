92b0ce72c2b34fa1d333239a8712032604f622ca
Alena Svetasheva <132587708+AlenaSv99@users.noreply.github.com>
2024-01-18 17:01:27 +0300

    Add files via upload

---
+++ Условия/Проверка на равенство в JavaScript (⊗jsPmCdECh)/script.js
@@ -0,0 +1,9 @@
+// Проверьте, что переменная test равна 10.
+
+let test = Number(prompt('Введите число'));
+
+if (test == 10) {
+	console.log(String(test) + ' равно 10'); 
+} else {
+	console.log(String(test) + ' не равно 10');
+}
